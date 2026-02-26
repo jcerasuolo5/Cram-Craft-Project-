b0f9cb1822fbbc83491561e2862b6b0e51fc1577
yohannanjk <jkyohannan@csu.fullerton.edu>
2026-02-26 14:12:01 -0800

    Installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-teal': '#6dbfb8',
+        'brand-dark': '#2d334a',
+      }
+    },
+  },
+  plugins: [],
+}
